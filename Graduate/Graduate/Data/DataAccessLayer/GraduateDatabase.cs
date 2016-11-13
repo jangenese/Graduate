210910d4e0ddaae4e8ca9f92a0546bf4d0047a74
jangenese <jangenese@gmail.com>
2016-11-12 22:33:49 -0700

    Removed Old DAL

--- Graduate/Graduate/Data/DataAccessLayer/GraduateDatabase.cs
+++ Graduate/Graduate/Data/DataAccessLayer/GraduateDatabase.cs
@@ -19,11 +19,14 @@ namespace Graduate.Core.Data.DataAccessLayer
             connection.DropTable<SchoolYear>();
             connection.DropTable<Semester>();
             connection.DropTable<Class>();
+            connection.DropTable<Grade>();
+            
 
             connection.CreateTable<SchoolYear>();
             connection.CreateTable<Semester>();
             connection.CreateTable<Class>();
-            
+            connection.CreateTable<Grade>();
+
         }
 
 
