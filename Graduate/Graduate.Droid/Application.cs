b95f3d825ec436ac031fa1de599783f051662636
jangenese <jangenese@gmail.com>
2016-12-06 19:57:43 -0700

    First Run Check working

--- Graduate/Graduate.Droid/Application.cs
+++ Graduate/Graduate.Droid/Application.cs
@@ -3,15 +3,17 @@ using System.IO;
 using SQLite;
 using Android.App;
 using Graduate.Core;
-
+using Android.Content;
 
 namespace Graduate.Droid
 {
     [Application]
     public class GraduateApp : Application
     {
+        private Boolean firstRun;
+        ISharedPreferences prefs = null;
         public static GraduateApp Current { get; private set; }
-        
+
         public Planner planner { get; set; }
         public GradeConverter converter { get; set; }
         SQLiteConnection conn;
@@ -25,19 +27,50 @@ namespace Graduate.Droid
         {
             base.OnCreate();
 
+            prefs = Application.Context.GetSharedPreferences("Graduate.Droid", FileCreationMode.Private);
+
             var sqliteFilename = "Graduate.db3";
             string libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             var path = Path.Combine(libraryPath, sqliteFilename);
             conn = new SQLiteConnection(path);
 
 
-          //  calculator = new Calculator(conn);
+            //  calculator = new Calculator(conn);
             planner = new Planner(conn);
 
             converter = new GradeConverter(conn);
-            
+            firstRun = prefs.GetBoolean("firstRun", true);
+
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n"); Console.WriteLine("First run check\n"); Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+            Console.WriteLine("First run check\n");
+
+            if (firstRun)
+            {
+
+                Console.WriteLine("Hi, this is my first run");
+
+
+                ISharedPreferencesEditor editor = prefs.Edit();
+                editor.PutBoolean("firstRun", false);
+                editor.Apply();
+            }
+
+            Console.WriteLine("Not a a first run");
+
 
         }
+
     }
 }
 
