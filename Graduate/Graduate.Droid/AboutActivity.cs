981d44347b5123ed771e51bc647629ffa9e795e5
jangenese <jangenese@gmail.com>
2016-12-06 20:32:31 -0700

    Change Title for overflow pages and added overflow menu

--- Graduate/Graduate.Droid/AboutActivity.cs
+++ Graduate/Graduate.Droid/AboutActivity.cs
@@ -18,9 +18,15 @@ namespace Graduate.Droid
         
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            this.Title = "About";
             base.OnCreate(savedInstanceState);
 
+
+
+
             SetContentView(Resource.Layout.AboutActivity);
+
+
 
             TextView t = (TextView)FindViewById(Resource.Id.textView2);
             t.Text = "App Version 1.00 \n\nDeveloped By Tough Cookie Team\nTerm Project For COMP3504 - MRU"
@@ -28,6 +34,29 @@ namespace Graduate.Droid
                 "For any questions or concerns please contact Tough Cookie at ToughCookie@gmail.com";
 
 
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main_menu, menu);
+            return base.OnPrepareOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+
+                case Resource.Id.menu_preferences:
+                    var preferenceIntent = new Intent(this, typeof(PreferencesActivity));
+                    StartActivity(preferenceIntent);
+                    break;
+                case Resource.Id.menu_about:
+                    var aboutIntent = new Intent(this, typeof(AboutActivity));
+                    StartActivity(aboutIntent);
+                    break;
+            }
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
